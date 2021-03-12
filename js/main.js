1ff10a7d3c95b4826b9fd310b762d2ac9b13834c
Teodor Madzovski <ted.madzovski@gmail.com>
2021-03-12 09:13:47 +0100

    13 commit

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
-$(document).ready(() => {
+ 
+ /*
+ //initial values
+ const api_key="98325a9d3ed3ec225e41ccc4d360c817";
+ 
+ const url="https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817";
+ 
+ //selecting elements from DOOM
+ const buttonElement=document.querySelector('#search');
+ const inputElement=document.querySelector('#select');
+
+
+ buttonElement.onclick= function (event){
+   event.preventDefault();
+   const value = inputElement.value;
+   const newUrl= url +'&query='+ value;
+  
+     
+    fetch( newUrl)
+
+     .then((res)=>res.json())
+     .then((data)=>{
+       console.log('Data',data);
+     })
+     .catch((error)=>{
+       console.log('Erro:',error);
+     });
+     console.log('Value' ,value)
+
+   
+ 
+ }*/
+
+ 
+ $(document).ready(() => {
   $("#searchForm").on('submit', (e) => {
     e.preventDefault();
     let searchText = $("#searchText").val();
+    
     getMovies(searchText);
   });
 });
 
 
-$(document).ready(() => {
-  $("#searchingType").on('click',(e) => {
-    e.preventDefault();
-    let searchbygenre = $("#select").val();
-    getMovieByGenre(searchbygenre);
-  });});
-  
-  
-$(document).ready(() => {
-  $("#searchFormbyNumber").on('submit',(e) => {
-    e.preventDefault();
-    let searchbyyear = $("#picker").val();
-    getMoviesByYear(searchbyyear);
-  });});
-  
 
-function getMoviesByYear(searchbyyear){
 
-  axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchbyyear )
-    .then(function (response) {
-      let movies = response.data.results;
-      let output = '';
-      $.each(movies, (index, movie) => {
-        output+=`
-          <div class="col-md-3">
-            <div class="well text-center">
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-              <h5>${movie.title}</h5>
-              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
-            </div>
-          </div>
-        `;
-      });
-      $('#movies').html(output);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 
-function getMovies(searchText){
-  
+
+function getMovies(searchText,){
+ 
   axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText )
     .then(function (response) {
       let movies = response.data.results;
@@ -72,38 +71,18 @@ function getMovies(searchText){
 }
 
 
-function   getMovieByGenre(searchbygenre){
-  
-  axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchbygenre )
-    .then(function (response) {
-      let movies = response.data.results;
-      let output = '';
-      $.each(movies, (index, movie) => {
-        output+=`
-          <div class="col-md-3">
-            <div class="well text-center">
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-              <h5>${movie.title}</h5>
-              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
-            </div>
-          </div>
-        `;
-      });
-      $('#movies').html(output);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 
 
- // $('#picker').datetimepicker({
- //  timepicker: false,
- //  datepicker: true,
- // format: 'Y-m-d H:i', // formatDate
-//  hours12: false,
- //  step: 1
- // })
+
+
+
+// $('#picker').datetimepicker({
+//   timepicker: false,
+//   datepicker: true,
+//   format: 'Y-m-d H:i', // formatDate
+//   hours12: false,
+//   step: 1
+// })
 
   
 
@@ -117,11 +96,9 @@ function movieSelected(id){
 
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
- 
   axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
     .then(function (response) {
     let movie = response.data;
-    
     let output = `
         <div class="row">
           <div class="col-md-4">
@@ -151,3 +128,7 @@ function getMovie(){
       console.log(error);
     });
 }
+ 
+ 
+
+
