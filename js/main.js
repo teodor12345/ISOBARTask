b11d9d4dae1b11354ece2b1429fe2e0ce84e5dcf
Teodor Madzovski <ted.madzovski@gmail.com>
2021-03-14 22:29:16 +0100

    24 commit

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@
                 <div class="well text-center">
                   <img src="${movie.Poster}">
                   <h5>${movie.Title}</h5>
-                  <a onclick="movieSelected('${movie.i}')" class="btn btn-primary" href="#">Movie Details</a>
+                  <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                 </div>
               </div>
             `;
@@ -63,29 +63,39 @@
   
 
 
-
-function movieSelected(id){
+function movieSelected (id){
   sessionStorage.setItem('movieId',id );
-  window.location = 'movie.html';
+  window.location = 'Movie.html';
   return false;
 }
 
 
-function getMovieInfo(){
-  const api_key="f574ff9";
-  const url = `http://www.omdbapi.com/?apikey=${api_key}`; 
-  axios.get( url  )
-    .then(function (response) { 
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie(){
+  let movieId = sessionStorage.getItem('movieId');
+  // Make a request for a user with a given ID
+  axios.get("http://www.omdbapi.com/" + movieId + "?api_key=f574ff9")
+    .then(function (response) {
     let movie = response.data;
+    //console.log(movie);
     let output = `
         <div class="row">
           <div class="col-md-4">
-            <img src="${movie.Poster}" class="thumbnail">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
           </div>
           <div class="col-md-8">
-            <h2>${movie.Title}</h2>
+            <h2>${movie.title}</h2>
             <ul class="list-group">
-              <li class="list-group-item"><strong>Released:</strong> ${movie.Release_date}</li>
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+              <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
+              <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
             </ul>
           </div>
         </div>
@@ -94,6 +104,7 @@ function getMovieInfo(){
             <h3>Plot</h3>
             ${movie.overview}
             <hr>
+            <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
             <a href="index.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
@@ -104,7 +115,5 @@ function getMovieInfo(){
       console.log(error);
     });
 }
- 
- 
 
 
